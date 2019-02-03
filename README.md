5c41836c82f2d56f8aea05fc741f61b94e5a138d
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-02-03 07:35:10 +0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-## This is just an experimental app
-### made for education purpose, feel free to experiment with it. Thank you
---
+++ precache-manifest.1c95ce64a1424dc5dc2a5a45567167df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "80d374b2c63e730f6714ad1773631eb2",
+    "url": "/react-facedetection-app/static/media/little-cat-vector.80d374b2.png"
+  },
+  {
+    "revision": "9d32b930550841c3387d",
+    "url": "/react-facedetection-app/static/js/runtime~main.9d32b930.js"
+  },
+  {
+    "revision": "744d92ed0f0803c05504",
+    "url": "/react-facedetection-app/static/js/main.744d92ed.chunk.js"
+  },
+  {
+    "revision": "953fbd50c20cc223aaa7",
+    "url": "/react-facedetection-app/static/js/1.953fbd50.chunk.js"
+  },
+  {
+    "revision": "744d92ed0f0803c05504",
+    "url": "/react-facedetection-app/static/css/main.4e18fc52.chunk.css"
+  },
+  {
+    "revision": "953fbd50c20cc223aaa7",
+    "url": "/react-facedetection-app/static/css/1.352114d6.chunk.css"
+  },
+  {
+    "revision": "a01b46e423e9f47f59742079d23701fb",
+    "url": "/react-facedetection-app/index.html"
+  }
+];
